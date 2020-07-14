40850ac6171681f7026054931cc970db6530a06f
Sagor31h2 <sagor31h2@gmail.com>
2020-07-14 21:35:07 +0600

    Components part one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,23 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from "./app.component";
 import { from } from "rxjs";
 
 import "hammerjs";
+import { MenuComponent } from "./menu/menu.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
