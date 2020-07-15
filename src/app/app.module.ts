2b81b31297366d9b7809b11037bebab6032d0083
Sagor31h2 <sagor31h2@gmail.com>
2020-07-16 00:47:20 +0600

    Adding header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,11 @@ import { from } from "rxjs";
 import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { DishdetialComponent } from "./dishdetial/dishdetial.component";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetialComponent],
+  declarations: [AppComponent, MenuComponent, DishdetialComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
