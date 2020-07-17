a685f04990bf33f9a1a92f2c255de2345896521c
Sagor31h2 <sagor31h2@gmail.com>
2020-07-17 15:05:56 +0600

    About us and home ceo cart edited

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { LeaderService } from "./../services/leader.service";
+import { LEADERS } from "./../shared/leaders";
+import { Component, OnInit } from "@angular/core";
+import { Leader } from "../shared/leader";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  leaders: Leader[];
+  constructor(private leaderServic: LeaderService) {}
 
   ngOnInit() {
+    this.leaders = this.leaderServic.getLeaders();
   }
-
 }
